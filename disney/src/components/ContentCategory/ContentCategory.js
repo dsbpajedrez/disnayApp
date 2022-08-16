e5725a0eb055f8f898b87c0e4b3d5cc9124f7ce8
David Santiago Benjumea Perez <davidbenjumchess@gmail.com>
2022-08-16 12:27:32 -0500

    new: query for marvel collection

--- disney/src/components/ContentCategory/ContentCategory.js
+++ disney/src/components/ContentCategory/ContentCategory.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import {getDoc, getFirestore,doc, where, getDocs, query, collection} from 'firebase/firestore'
 
 const ContentCategory = () => {
+  const [movies,setMovies] =useState([])
+  const id = useParams().id;
+  
+  useEffect(()=>{
+    const db = getFirestore()
+    const q=query(
+      collection(db,'peliculas'),
+      where('category','==','Marvel')
+      )
+    getDocs(q)
+      .then(snapshot=>{
+        setMovies(
+          snapshot.docs.map(item=>(
+            {
+            id:item.id,
+            ...item.data()
+          }
+          
+          ))
+        )
+    
+      })
+  },[])
+  console.log(movies??[]);
   return (
     <div>ContentCategory</div>
   )
