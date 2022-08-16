77d02c306a5700c397b196bfd7a60578d839a37f
David Santiago Benjumea Perez <davidbenjumchess@gmail.com>
2022-08-16 12:26:13 -0500

    fix: Naming again component main to home

--- disney/src/App.js
+++ disney/src/App.js
@@ -2,14 +2,14 @@
 import './App.css';
 import LayOut from './LayuOut/LayOut';
 import Header from './components/Header/Header';
-import Main from './components/Main/Main';
+import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 
 function App() {
   return (
 <LayOut>
   <Header/>
-  <Main/>
+  <Home/>
   <Footer/>
 </LayOut>
   );
